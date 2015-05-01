4532335a7e79ec748432feb5365fc727db5de3cd
HAM1DR37A <pandora10mk@gmail.com>
2015-05-01 14:59:21 +0430

    commiting these files

---
+++ APProject/src/Bank.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+
+public class Bank {
+	private HashMap<Player, Double> accounts= new HashMap<Player, Double>();
+	private HashMap<Player , Double> debts = new HashMap<Player, Double>();
+	public static void addPlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void increaseBalance(Player aPlayer,Double amount){
+		//TODO	
+	}
+	public static void decreaseBalance(Player aPlayer,Double amount){
+		//TODO
+	}
+	public static double getBalance(Player aPlayer){
+		return 0;
+		//TODO
+	}
+
+}
